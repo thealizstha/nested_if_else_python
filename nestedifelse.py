d69e75f62ed42a7fb674b9faaa75b10381eaf14e
Aliz Shrestha <98611497+thealizstha@users.noreply.github.com>
2023-11-15 21:51:54 +0545

    nested_if_else

---
+++ nestedifelse.py
@@ -0,0 +1,12 @@
+# nested if-elif condition example
+p=67
+if p>=90:
+    print('distiction')
+elif p>=80:
+        print('first div')
+elif p>=70:
+        print('second div')
+elif p>=60:
+        print('pass')
+else:
+     print('fail')
